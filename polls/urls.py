f7dc04876b4656b07642e9eb6448daddf0282205
Tezigudo <godjangg@gmail.com>
2022-09-03 16:35:12 +0700

    finished django part3 #3

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/time', views.showtime, name='time')
+    path('time/', views.showtime, name='time'),
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.utils import timezone
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hello its me ')
+    last_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template method
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'lastest_question_list': last_question_list
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def showtime(request) -> HttpResponse:
@@ -12,3 +22,20 @@ def showtime(request) -> HttpResponse:
     msg = f"<p>The time is {thaitime}.</p>"
     # return the msg in an HTTP response
     return HttpResponse(msg)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question doesn't exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking result of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting at question {question_id}")
