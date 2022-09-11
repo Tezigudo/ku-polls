1c4e6a4d6fe50ec0a98fa6fa8c61f137c6f3dbe3
Tezigudo <godjangg@gmail.com>
2022-09-11 17:42:35 +0700

    😎 Fixed bug and close #22

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date ended', null=True)
+    end_date = models.DateTimeField('date ended', null=True, blank=True)
 
     @admin.display(
         boolean=True,
