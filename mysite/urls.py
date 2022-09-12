ce83ad622115c0d0a11ccf06c2ee88aa8c29e110
Tezigudo <godjangg@gmail.com>
2022-09-12 21:24:29 +0700

    💻Fixed #24 somehow it works xD🥳

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect('polls/'), name='Redirecting'),
+    path('', lambda request: HttpResponseRedirect('/polls/'), name='Redirecting'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
