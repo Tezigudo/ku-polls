60f17265eba328d5e17e0de7235588b3f7c52ca4
Tezigudo <godjangg@gmail.com>
2022-09-15 15:03:12 +0700

    config some auth

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.http import HttpResponseRedirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect('/polls/'), name='Redirecting'),
+    path('', RedirectView.as_view(url='/polls/'), name='Redirecting'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
